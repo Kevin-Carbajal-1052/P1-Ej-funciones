3a97a3b4072c316284ea3d5c2bbea1e4e257c4c7
Kevin-Carbajal-1052 <164533993+Kevin-Carbajal-1052@users.noreply.github.com>
2025-02-07 16:48:08 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,16 @@
+void saludar(){
+  print('hola mundo');
+}
+
+void escribeNombre(){
+  print('Kevin');
+}
+void main(){
+  //Kevin Carbajal - 22308051281052
+  //llamar a la funcion saludar
+  print('Kevin Carbajal - 22308051281052');
+  print('llamar a la funcion saludar');
+  saludar();
+  print('Llamando a la funcion escribeNombre');
+  escribeNombre();
+}
