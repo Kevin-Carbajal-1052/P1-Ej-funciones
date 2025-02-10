ee21cba1aae0420ca603714cc328b6f37010221f
Kevin-Carbajal-1052 <164533993+Kevin-Carbajal-1052@users.noreply.github.com>
2025-02-10 18:04:24 +0000

    ejemplo funcion rama

---
+++ funcion2.dart
@@ -0,0 +1,12 @@
+void suma(int a, int b){
+  print('La suma de $a + $b es: ${a+b}');
+}
+void resta(int a, int b){
+  print('La resta de $a - $b es: ${a-b}');
+}
+void main (){
+  print("Kevin Carbajal - 22308051281052");
+  print("LLamando a la funcion suma y resta");
+  suma(10, 5);
+  resta(10, 5);
+}
